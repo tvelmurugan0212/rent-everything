bd580d985773554be557c58520bc1c902ece65ab
velmurugan <velmurugan@letsdosimple.com>
2026-08-18 13:22:32 +0530

    commit

--- rent_everything/lib/main.dart
+++ rent_everything/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:rent_everything/firebase_options.dart';
 
 import 'my_routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
